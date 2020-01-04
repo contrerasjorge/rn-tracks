5740fea7641732a59494892c067d81976a91b7e1
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-04 13:02:28 -0500

    Created User Schema and implemented JWT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+require('./models/User');
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
+
+app.use(express.json());
+app.use(authRoutes);
 
 const mongoUri = process.env.MONGO;
 
@@ -19,8 +26,8 @@ mongoose.connection.on('error', err => {
   console.error('Error connecting to mongo 😫', err);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
